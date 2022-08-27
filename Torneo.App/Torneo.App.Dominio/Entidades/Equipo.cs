c00e264fd546816ef8a76fa3bc59991723f5cd23
Washington <washington_nieto@hotmail.com>
2022-08-27 10:16:11 -0500

    primer commit

---
+++ Torneo.App/Torneo.App.Dominio/Entidades/Equipo.cs
@@ -0,0 +1,12 @@
+//cambios
+
+namespace Torneo.App.Dominio
+{
+    public class Equipo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public Municipio Municipio { get; set; }
+        public DirectorTecnico DirectorTecnico { get; set; }
+    }
+}
