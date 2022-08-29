c37ea5458ca6668b653274e93b9025cf47e0aee7
Wnieto <washington_nieto@hotmail.com>
2022-08-29 17:20:01 -0500

    Cambio bdatos - manizales

---
+++ Torneo.App/Torneo.App.Consola/Program.cs
@@ -0,0 +1,25 @@
+﻿using Torneo.App.Dominio;
+using Torneo.App.Persistencia;
+
+//actualizacion
+namespace Torneo.App.Consola
+{
+    class Program
+    {
+        private static IRepositorioMunicipio _repoMunicipio = new RepositorioMunicipio();
+
+        static void Main(string[] args) 
+        {
+            AddMunicipio();
+         }
+
+        private static void AddMunicipio()
+        {
+            var municipio = new Municipio 
+            { 
+                Nombre = "Manizales", 
+            };
+            _repoMunicipio.AddMunicipio(municipio);
+        }
+    }
+}
