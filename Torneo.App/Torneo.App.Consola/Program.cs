8c53d10eb98a98243d15fee9b66350cd4d72eb6e
Wnieto <washington_nieto@hotmail.com>
2022-09-02 15:39:36 -0500

    Menu DT consola

--- Torneo.App/Torneo.App.Consola/Program.cs
+++ Torneo.App/Torneo.App.Consola/Program.cs
@@ -2,24 +2,60 @@
 using Torneo.App.Persistencia;
 
 //actualizacion
+//mdoificacion Washington Nieto 
 namespace Torneo.App.Consola
 {
     class Program
     {
         private static IRepositorioMunicipio _repoMunicipio = new RepositorioMunicipio();
+        private static IRepositorioDT _repoDT = new RepositorioDT();
 
         static void Main(string[] args) 
         {
-            AddMunicipio();
+            int opcion = 0;
+            do{
+                Console.WriteLine("1 Insert Municipio");
+                Console.WriteLine("2 Insert Director Tecnico");
+                Console.WriteLine("0 Salir");
+
+                opcion = Int32.Parse(Console.ReadLine());
+                switch(opcion)
+                {
+                    case 1:
+                        AddMunicipio();
+                        break;
+                    case 2:
+                        AddDT();
+                        break;
+                }
+            }while(opcion != 0);
          }
 
         private static void AddMunicipio()
         {
+            Console.WriteLine("Ingrese el nombre del municipio");
+            string nombre = Console.ReadLine();
             var municipio = new Municipio 
             { 
-                Nombre = "Manizales", 
+                Nombre = nombre, 
             };
             _repoMunicipio.AddMunicipio(municipio);
+        }
+        private static void AddDT()
+        {
+            Console.WriteLine("Ingrese el nombre del director tecnico");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el documento del director tecnico");
+            string documento = Console.ReadLine();
+            Console.WriteLine("Ingrese el telefono del director tecnico");
+            string telefono = Console.ReadLine();
+            var directorTecnico = new DirectorTecnico 
+            { 
+                Nombre = nombre, 
+                Documento = documento, 
+                Telefono = telefono, 
+            };
+            _repoDT.AddDT(directorTecnico);
         }
     }
 }
--- Torneo.App/Torneo.App.Persistencia/AppRepositorios/DataContext.cs
+++ Torneo.App/Torneo.App.Persistencia/AppRepositorios/DataContext.cs
@@ -6,15 +6,10 @@ namespace Torneo.App.Persistencia
     public class DataContext : DbContext
     {
         public DbSet<Municipio> Municipios { get; set; }
-
         public DbSet<DirectorTecnico> DirectoresTecnicos { get; set; }
-
         public DbSet<Equipo> Equipos { get; set; }
-
         public DbSet<Partido> Partidos { get; set; }
-
         public DbSet<Posicion> Posiciones { get; set; }
-
         public DbSet<Jugador> Jugadores { get; set; }
 
         protected override void OnConfiguring(
