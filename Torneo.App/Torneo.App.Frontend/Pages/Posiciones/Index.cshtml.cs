a33ea089b401341cec00e783833a90acf36cc5cf
washington_nieto@hotmail.com <washington_nieto@hotmail.com>
2022-09-09 20:19:17 -0500

    Actualizacion Washington

---
+++ Torneo.App/Torneo.App.Frontend/Pages/Posiciones/Index.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Torneo.App.Persistencia;
+using Torneo.App.Dominio;
+
+namespace Torneo.App.Frontend.Pages.Posiciones
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
