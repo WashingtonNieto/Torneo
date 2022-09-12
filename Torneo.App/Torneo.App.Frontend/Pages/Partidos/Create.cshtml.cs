c3ea9516e2d1237eb36960e7eec85ca85931169e
Wnieto <washington_nieto@hotmail.com>
2022-09-12 17:24:37 -0500

    Creacion partido

--- Torneo.App/Torneo.App.Frontend/Pages/Partidos/Create.cshtml.cs
+++ Torneo.App/Torneo.App.Frontend/Pages/Partidos/Create.cshtml.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Torneo.App.Dominio;
+using Torneo.App.Persistencia;
 
 namespace Torneo.App.Frontend.Pages.Partidos
 {
     public class CreateModel : PageModel
     {
+        private readonly IRepositorioPartido _repPartodp;
+        private readonly IRepositorioEquipo _repoEquipo;
+
+        public Partido partido {get; set;}
+        public IEnumerable<Equipo> equipos { get; set; }
+
+        public CreateModel(IRepositorioPartido repoPartido, IRepositorioEquipo repoEquipo)
+
+        {
+            _repoPartido = repoPartido;
+            _repoEquipo = repoMunicipio;
+        }
+
         public void OnGet()
         {
+            partido = new Partido();
+            equipos = _repoEquipo.GetAllEquipos();
         }
     }
 }
