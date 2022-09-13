4c47512abea77e811ea05b971f7a657208d48159
washington_nieto@hotmail.com <washington_nieto@hotmail.com>
2022-09-12 22:10:33 -0500

    Partidos crear - error en la base de datos -

--- Torneo.App/Torneo.App.Frontend/Pages/Partidos/Create.cshtml.cs
+++ Torneo.App/Torneo.App.Frontend/Pages/Partidos/Create.cshtml.cs
@@ -5,25 +5,31 @@ using Torneo.App.Persistencia;
 
 namespace Torneo.App.Frontend.Pages.Partidos
 {
-    public class CreateModel : PageModel
+  public class CreateModel : PageModel
+  {
+    private readonly IRepositorioPartido _repoPartido;
+    private readonly IRepositorioEquipo _repoEquipo;
+
+    public Partido partido { get; set; }
+    public IEnumerable<Equipo> equipos { get; set; }
+
+    public CreateModel(IRepositorioPartido repoPartido, IRepositorioEquipo repoEquipo)
+
     {
-        private readonly IRepositorioPartido _repPartodp;
-        private readonly IRepositorioEquipo _repoEquipo;
-
-        public Partido partido {get; set;}
-        public IEnumerable<Equipo> equipos { get; set; }
-
-        public CreateModel(IRepositorioPartido repoPartido, IRepositorioEquipo repoEquipo)
-
-        {
-            _repoPartido = repoPartido;
-            _repoEquipo = repoMunicipio;
-        }
-
-        public void OnGet()
-        {
-            partido = new Partido();
-            equipos = _repoEquipo.GetAllEquipos();
-        }
+      _repoPartido = repoPartido;
+      _repoEquipo = repoEquipo;
     }
+
+    public void OnGet()
+    {
+      partido = new Partido();
+      equipos = _repoEquipo.GetAllEquipos();
+    }
+
+    public IActionResult OnPost(Partido partido, DateTime FechaHora, int idEquipoLocal, int marcadorLocal, int idEquipoVisitante, int marcadorVisitante)
+    {
+      _repoPartido.AddPartido(partido, FechaHora, idEquipoLocal, marcadorLocal, idEquipoVisitante, marcadorVisitante);
+      return RedirectToPage("Index");
+    }
+  }
 }
