731672571c3e69b415b2dfcc426d3c641f57eb2f
washington_nieto@hotmail.com <washington_nieto@hotmail.com>
2022-09-14 09:10:57 -0500

    Actualizar municipios

---
+++ Torneo.App/Torneo.App.Frontend/Pages/DirectoresTecnicos/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Torneo.App.Frontend.Pages.DirectoresTecnicos
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
