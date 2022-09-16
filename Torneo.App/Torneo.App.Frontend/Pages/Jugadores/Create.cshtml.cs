3d53ce5a97b1250e60a9175f194efea807ef6882
washington_nieto@hotmail.com <washington_nieto@hotmail.com>
2022-09-16 09:40:11 -0500

    Editar Jugadores
    Se edita jugadore- falta equipo

--- Torneo.App/Torneo.App.Frontend/Pages/Jugadores/Create.cshtml.cs
+++ Torneo.App/Torneo.App.Frontend/Pages/Jugadores/Create.cshtml.cs
@@ -1,4 +1,7 @@
-using Microsoft.ModelValidate.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Torneo.App.Dominio;
