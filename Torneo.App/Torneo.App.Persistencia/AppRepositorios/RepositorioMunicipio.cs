d9625d7e7b228c5b0678721bd97c623340bfc34b
Washington Nieto <washington_nieto@hotmail.com>
2022-09-22 10:53:34 -0500

    Boton municipios borrar 2

--- Torneo.App/Torneo.App.Persistencia/AppRepositorios/RepositorioMunicipio.cs
+++ Torneo.App/Torneo.App.Persistencia/AppRepositorios/RepositorioMunicipio.cs
@@ -1,48 +1,52 @@
 using Microsoft.EntityFrameworkCore;
 using Torneo.App.Dominio;
+
 namespace Torneo.App.Persistencia
 {
-  public class RepositorioMunicipio : IRepositorioMunicipio
-  {
-    private readonly DataContext _dataContext = new DataContext();
-    public Municipio AddMunicipio(Municipio municipio)
+    public class RepositorioMunicipio : IRepositorioMunicipio
     {
-      var municipioInsertado = _dataContext.Municipios.Add(municipio);
-      _dataContext.SaveChanges();
-      return municipioInsertado.Entity;
-    }
+        private readonly DataContext _dataContext = new DataContext();
 
-    public IEnumerable<Municipio> GetAllMunicipios()
-    {
-      return _dataContext.Municipios;
-    }
+        public Municipio AddMunicipio(Municipio municipio)
+        {
+            var municipioInsertado = _dataContext.Municipios.Add(municipio);
+            _dataContext.SaveChanges();
+            return municipioInsertado.Entity;
+        }
 
-    public Municipio GetMunicipio(int idMunicipio)
-    {
-      var municipioEncontrado = _dataContext.Municipios.Find(idMunicipio);
-      return municipioEncontrado;
-    }
+        public IEnumerable<Municipio> GetAllMunicipios()
+        {
+            var municipios = _dataContext.Municipios.Include(m => m.Equipos).ToList();
 
-    public Municipio UpdateMunicipio(Municipio municipio)
-    {
-      var municipioEncontrado = _dataContext.Municipios.Find(municipio.Id);
-      if (municipioEncontrado != null)
-      {
-        municipioEncontrado.Nombre = municipio.Nombre;
-        _dataContext.SaveChanges();
-      }
-      return municipioEncontrado;
-    }
-    public Municipio DeleteMunicipio(int idMunicipio)
-    {
-      var municipioEncontrado = _dataContext.Municipios.Find(idMunicipio);
-      if (municipioEncontrado != null)
-      {
-        _dataContext.Municipios.Remove(municipioEncontrado);
-        _dataContext.SaveChanges();
-      }
-      return municipioEncontrado;
-    }
+            return municipios;
+        }
 
-  }
+        public Municipio GetMunicipio(int idMunicipio)
+        {
+            var municipioEncontrado = _dataContext.Municipios.Find(idMunicipio);
+            return municipioEncontrado;
+        }
+
+        public Municipio UpdateMunicipio(Municipio municipio)
+        {
+            var municipioEncontrado = _dataContext.Municipios.Find(municipio.Id);
+            if (municipioEncontrado != null)
+            {
+                municipioEncontrado.Nombre = municipio.Nombre;
+                _dataContext.SaveChanges();
+            }
+            return municipioEncontrado;
+        }
+
+        public Municipio DeleteMunicipio(int idMunicipio)
+        {
+            var municipioEncontrado = _dataContext.Municipios.Find(idMunicipio);
+            if (municipioEncontrado != null)
+            {
+                _dataContext.Municipios.Remove(municipioEncontrado);
+                _dataContext.SaveChanges();
+            }
+            return municipioEncontrado;
+        }
+    }
 }
